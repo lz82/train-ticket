35e4684a7b7603a006458e17b21ff3ac5f31a719
liuzhuang <70458055@qq.com>
2019-12-18 17:57:30 +0800

    :construction: pro init

--- src/app.jsx
+++ src/app.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import './app.css'
 
-import TodoList from './pages/todolist'
-
 function App() {
   return (
     <div className="app">
-      <TodoList />
+      app
     </div>
   )
 }
